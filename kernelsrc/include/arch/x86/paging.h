553d1f542406cebe864793259f5feea9e101d9a7
NateDogg1232 <nathanielbarragan@gmail.com>
2017-12-15 00:41:39 -0800

    Added skeleton for paging
    I misread all the paging stuff but I can say for a fact that won't be happening anymore

---
+++ kernelsrc/include/arch/x86/paging.h
@@ -0,0 +1,9 @@
+#ifndef PAGING_H
+#define PAGING_H 1
+#include <common.h>
+
+uint16_t set_pte_flags(bool p, bool rw, bool us, bool pwt, bool pcd, bool a, bool d, bool pat, bool g);
+
+uint16_t set_pte(uint32_t addr, uint16_t flags);
+
+#endif
---
+++ kernelsrc/kernel/arch/x86/paging.c
@@ -0,0 +1,12 @@
+#include <arch/x86/paging.h>
+
+uint16_t set_pte_flags(bool p, bool rw, bool us, bool pwt, bool pcd, bool a, bool d, bool pat, bool g)
+{
+    
+    return 0;
+}
+
+uint16_t set_pte(uint32_t addr, uint16_t flags)
+{
+    return 0;
+}
